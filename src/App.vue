26060e1ad27210612a5b2c5db9f1516a10b9aec9
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-09-15 00:49:39 -0500

    End of video part one changes, to do list properly clears when button is clicked

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,25 @@
 
 import {ref} from "vue"
 
-const name =ref('')
+const myArray = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category =ref(null)
+
+const addToDo =() => {
+  if (input_content.value.trim() ===''|| input_category.value == null){
+    return
+  }
+
+  myArray.value.push({
+    content: input_content.value,
+    catagory: input_category.value,
+  })
+
+  input_content.value = ''
+  input_category= null
+
+}
 
 </script>
 
@@ -15,7 +33,32 @@ const name =ref('')
 
     </section>
 <section class="create-todo">
+<h3>
+  CREATE A TO DO LIST
+</h3>
 
+<form @submit.prevent= "addToDo">
+  <h4>What's on your to do list?</h4>
+    <input type="text" placeholder ="e.g. Make a video" v-model="input_content"/>
+
+  <h4>Pick a catagory</h4>
+    <div>
+      <label>
+        <input type = "radio" name = "category" value = "business" v-model= "input_category">
+        <span class="bubble business"></span>
+        <div>Business</div>
+      </label>
+
+      <label>
+        <input type = "radio" name = "category" value = "personal" v-model= "input_category">
+        <span class="bubble personal"></span>
+        <div>Personal</div>
+      </label>
+  </div>
+
+  <input type="submit" value="Add to List">
+
+</form>
 </section>
 
 <section class="todo-list">
